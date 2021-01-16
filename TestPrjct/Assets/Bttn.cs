f3c01048c5915585afa4e45a87bc2669ceeaea27
GolczA <66203141+GolczA@users.noreply.github.com>
2021-01-16 17:13:02 +0300

    Main Information
    Закомитил проект юнити

---
+++ TestPrjct/Assets/Bttn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bttn : MonoBehaviour
+{
+    public Text tx;
+    int count;
+
+    private void Start()
+    {
+        count = 0;
+    }
+    private void Update()
+    {
+        tx.text = count.ToString();
+    }
+
+    public void PlusBttn()
+    {
+        count += 1;
+    }
+    public void MunusBttn()
+    {
+        count -= 1; 
+    }
+}
